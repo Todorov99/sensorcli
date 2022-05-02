61884c4a1acc0bd51f8214cd6d53053a2282585b
Todorov99 <toshko_m1999@abv.bg>
2022-05-02 21:56:06 +0300

    Implement mailsender client

--- pkg/client/types.go
+++ pkg/client/types.go
@@ -4,3 +4,10 @@ type UserDto struct {
 	Username string `json:"username,omitempty"`
 	Password string `json:"password,omitempty"`
 }
+
+type MailSender struct {
+	Subject string   `json:"subject,omitempty"`
+	Cc      []string `json:"cc,omitempty"`
+	To      []string `json:"to,omitempty"`
+	Body    string   `json:"body,omitempty"`
+}
